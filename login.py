92984ffb8cf986a03042ac27d8f4070e40de413e
FTDaily <52481063+ftdaily@users.noreply.github.com>
2024-02-28 16:16:40 +0700

    Add files via upload

---
+++ login.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+
+#Home Page daripada login
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+#Untuk mendapatkan input user dalam login
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+
+    if username == 'admin' and password == 'admin000':
+        return redirect(url_for('success'))
+    else:
+        return render_template('login.html', message='Username dan password salah atau tidak valid!')
+
+#Page jika login berhasil, maka akan berubah routingnya
+@app.route('/success')
+def success():
+    return "Login berhasil, selamat datang, Administrator"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
